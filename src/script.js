04c226be8be5e372f0814d572978815ab0cb6b3b
outofs <rtranscendental@gmail.com>
2022-07-16 23:14:29 +0300

    Adding first elements, nav menu, img, media

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const navMenu = document.querySelector(".nav__menu");
+const navToggle = document.querySelector(".nav__toggle");
+const navClose = document.querySelector(".nav__close");
+
+// console.log(navMenu, navToggle, navClose);
+
+/// Show or close  menu
+const showOrCloseMenu = function () {
+  navMenu.classList.toggle("show-menu");
+};
+
+if (navToggle) {
+  navToggle.addEventListener("click", showOrCloseMenu);
+}
+
+if (navClose) {
+  navClose.addEventListener("click", showOrCloseMenu);
+}
+
+/// Remove menu mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.querySelector(".nav__menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
